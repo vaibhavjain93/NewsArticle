30c6e3f20e8eb5cb4d7aa6f6f8ae0c3070ad1abd
vaibhav <vaibhav93.jain@gmail.com>
2019-04-26 22:49:17 +0530

    loader

--- app/src/main/java/com/example/news/utils/Constants.java
+++ app/src/main/java/com/example/news/utils/Constants.java
@@ -6,4 +6,6 @@ public class Constants {
     public static final String URL = "url";
     public static final String TITLE = "title";
     public static final String DESCRIPTION = "description";
+    public static final String[] INTRO_ARR = new String[]{"Personalizing Feeds...",
+            "Fetching Latest News for you...","Filtering must reads...","Building UI..."};
 }
--- app/src/main/java/com/example/news/views/fragment/NewsListFragment.java
+++ app/src/main/java/com/example/news/views/fragment/NewsListFragment.java
@@ -3,22 +3,28 @@ package com.example.news.views.fragment;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.news.R;
 import com.example.news.listeners.NewsItemClickListener;
 import com.example.news.model.NewsListObject;
+import com.example.news.utils.Constants;
 import com.example.news.views.viewmodel.NewsListViewModel;
 import com.example.news.views.viewmodel.NewsMainViewModel;
 import com.example.news.views.adapter.NewsListAdapter;
+
+import java.util.Random;
 
 public class NewsListFragment extends Fragment implements NewsItemClickListener, SwipeRefreshLayout.OnRefreshListener {
 
@@ -27,6 +33,9 @@ public class NewsListFragment extends Fragment implements NewsItemClickListener,
     private RecyclerView recyclerView;
     private NewsListAdapter newsListAdapter;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private View loadContainer;
+    private Handler loadHandler;
+    private TextView loadText;
 
     public static NewsListFragment newInstance() {
         return new NewsListFragment();
@@ -38,11 +47,36 @@ public class NewsListFragment extends Fragment implements NewsItemClickListener,
         View root = inflater.inflate(R.layout.news_list_fragment, container, false);
         recyclerView = root.findViewById(R.id.recycler_news);
         swipeRefreshLayout = root.findViewById(R.id.swipe_refresh);
+        loadContainer = root.findViewById(R.id.load_container);
         swipeRefreshLayout.setOnRefreshListener(this);
         newsListAdapter = new NewsListAdapter(getContext(),this);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(newsListAdapter);
+        loadHandler = new Handler();
+        loadText = root.findViewById(R.id.load_text);
         return root;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        startLoadHandler();
+    }
+
+    void startLoadHandler() {
+        loadHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.i(TAG, "run: ");
+                setLoadText();
+                startLoadHandler();
+            }
+        },700);
+    }
+
+    private static final String TAG = "201102387 NewsListFr";
+    void setLoadText() {
+        loadText.setText(Constants.INTRO_ARR[(new Random().nextInt(Constants.INTRO_ARR.length))]);
     }
 
     @Override
@@ -61,8 +95,11 @@ public class NewsListFragment extends Fragment implements NewsItemClickListener,
                         mViewModel.fetchDataFromDb();
                     }
                     else {
+                        loadContainer.setVisibility(View.GONE);
+                        loadHandler.removeCallbacksAndMessages(null);
                         newsListAdapter.setData(newsListObject.getNewsArrayList());
                         mViewModel.insertToDb(newsListObject.getNewsArrayList());
+
                     }
                 }
             }
